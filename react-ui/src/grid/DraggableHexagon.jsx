86f526892d1ce0b5039428b8f8dc271e2dd1280d
John Kryspin <johnkryspin@Johns-MBP.home>
2019-07-06 00:11:07 -0400

    reformatted sending data

--- react-ui/src/grid/DraggableHexagon.jsx
+++ react-ui/src/grid/DraggableHexagon.jsx
@@ -1,9 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { DropTarget, DragSource } from 'react-dnd'
 import Hexagon from 'react-hexagon';
-import { isatty } from 'tty';
+import _ from 'lodash';
 
-const HexagonTarget = ({ canDrop, isOver, connectDropTarget, imgSrc }) => {
+const HexagonTarget = ({ canDrop, isOver, connectDropTarget, connectDragSource, imgSrc, ...rest }) => {
     const isActive = canDrop && isOver
     let imageURL = null
     if (isActive) {
@@ -13,25 +13,39 @@ const HexagonTarget = ({ canDrop, isOver, connectDropTarget, imgSrc }) => {
     }
     imageURL = imageURL || imgSrc
     return (
-        <span ref={connectDropTarget}>
+        connectDragSource(<span ref={connectDropTarget}>
             <Hexagon backgroundImage={imageURL}>
             </Hexagon>
-        </span>
+        </span>)
     )
 
 
 }
-export default DropTarget(
-    "box",
-    {
-        drop: (props, monitor, component) => {
-            props.onDrop(monitor.getItem())
-        }
-    },
-    (connect, monitor) => ({
-        connectDropTarget: connect.dropTarget(),
-        isOver: monitor.isOver(),
-        canDrop: monitor.canDrop(),
-        didDrop: monitor.didDrop()
-    }),
-)(HexagonTarget)
+export default _.flow([
+    DragSource(
+        "box",
+        {
+            beginDrag: props => ({ name: props.name, keyName: props.keyName }),
+            // endDrag: props =>{
+            //     return {name: null, keyName: null}
+            // }
+        },
+        (connect, monitor) => ({
+            connectDragSource: connect.dragSource(),
+            isDragging: monitor.isDragging(),
+        }),
+    ), DropTarget(
+        "box",
+        {
+            drop: (props, monitor, component) => {
+                props.onDrop(monitor.getItem())
+            }
+        },
+        (connect, monitor) => ({
+            connectDropTarget: connect.dropTarget(),
+            isOver: monitor.isOver(),
+            canDrop: monitor.canDrop(),
+            didDrop: monitor.didDrop()
+        }),
+    )
+])(HexagonTarget)
--- react-ui/src/grid/DraggableHexagonContainer.jsx
+++ react-ui/src/grid/DraggableHexagonContainer.jsx
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from 'react'
 import DraggableHexagon from './DraggableHexagon';
 const DraggableHexagonContainer = (props) => {
     const [imgURL, setImgUrl]= useState(null)
+    const [champion, setChampion]= useState(null)
+
     const handleDrop = (champion) => {
-        console.log(champion)
+        setChampion(champion)
         setImgUrl(`http://ddragon.leagueoflegends.com/cdn/9.3.1/img/champion/${champion.keyName}.png`)
     }
-    return <DraggableHexagon onDrop={handleDrop} imgSrc={imgURL}></DraggableHexagon>
+    return <DraggableHexagon onDrop={handleDrop} imgSrc={imgURL} {...champion}></DraggableHexagon>
 }
 export default DraggableHexagonContainer
